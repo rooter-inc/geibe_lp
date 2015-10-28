075389a49946d7bcc2bf81e73401407e380fd296
tomomichi <tomomichi.onishi@gmail.com>
2015-10-28 17:04:02 +0900

    deploy setting

--- config.rb
+++ config.rb
@@ -70,3 +70,10 @@ configure :build do
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 end
+
+# Deploy
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.method = :git
+  deploy.branch = 'gh-pages'
+end
