3bd2a6a067fa22d03b6fb2e2be76c8245a1668d5
tomomichi <tomomichi.onishi@gmail.com>
2015-11-04 16:03:02 +0900

    use relative assets

--- config.rb
+++ config.rb
@@ -65,11 +65,11 @@ configure :build do
   # activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
-  activate :asset_host, :host => "/geibe_lp"
+  #activate :asset_host, :host => "/"
 end
 
 # Deploy
